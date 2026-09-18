32498d450d1faaf10e5813ca7e67dd6b3d20587f
Maciej Gajewski <gridsnap@zerodivisionerror.com>
2026-09-18 22:33:10 +0200

    gridsnap 0.1.1

---
+++ Casks/gridsnap.rb
@@ -0,0 +1,20 @@
+cask "gridsnap" do
+  version "0.1.1"
+  sha256 "aa09c504a7991f034bb7c38bcba1cb77dc15f53dfa243180df6a463418330709"
+
+  url "https://github.com/konieczkow/gridsnap/releases/download/v#{version}/GridSnap-#{version}.zip"
+  name "GridSnap"
+  desc "Grid-based window snapper, an open source alternative to Divvy"
+  homepage "https://github.com/konieczkow/gridsnap"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  depends_on macos: ">= :sonoma"
+
+  app "GridSnap.app"
+
+  zap trash: "~/Library/Preferences/com.zerodivisionerror.gridsnap.plist"
+end
---
+++ README.md
@@ -0,0 +1,7 @@
+# Homebrew tap
+
+```sh
+brew install --cask konieczkow/tap/gridsnap
+```
+
+Add `--no-quarantine` before `--cask` to skip the Gatekeeper prompt, since the app is not notarized.
