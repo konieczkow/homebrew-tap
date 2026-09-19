a5bb6b1d771e638da8d8a972f7576ff523433b98
Maciej Gajewski <gajewski.mm@gmail.com>
2026-09-19 20:46:18 +0200

    gridsnap 0.1.3

--- Casks/gridsnap.rb
+++ Casks/gridsnap.rb
@@ -1,6 +1,6 @@
 cask "gridsnap" do
-  version "0.1.2"
-  sha256 "41cd52c6b4832bc48a5abe92769109745a25ef618c7d27364cf05f3ddbc7265c"
+  version "0.1.3"
+  sha256 "48a2ab43b57721b3b918d653a39d6b2f064697215a96a818a99285369ef2f055"
 
   url "https://github.com/konieczkow/gridsnap/releases/download/v#{version}/GridSnap-#{version}.zip"
   name "GridSnap"
